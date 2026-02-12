b81fe3a4407c97980052076ce21b7f4a485f6d08
Oseimuohan Itua <oseimuohanitua@outlook.com>
2026-02-12 23:36:40 +0100

    Build from source to avoid release asset downloads

--- mailhog.rb
+++ mailhog.rb
@@ -4,31 +4,14 @@ class Mailhog < Formula
   version "2.0.0"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/OseimuohanI/MailHog/releases/download/v2.0.0/MailHog-darwin-arm64"
-      sha256 "b2925e51d0747cf947dc58d15700bad94dd7909320d072ee7081eb2e22879054"
-    else
-      url "https://github.com/OseimuohanI/MailHog/releases/download/v2.0.0/MailHog-darwin-amd64"
-      sha256 "e4b98f32a3443fb93ca3c736441c17e64d9d253a1330355b38549ecfbfb03b52"
-    end
-  end
+  url "https://github.com/OseimuohanI/MailHog.git",
+      revision: "0ff62423d8b76686622bdd5ba04489020f09f6db"
 
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/OseimuohanI/MailHog/releases/download/v2.0.0/MailHog-linux-arm64"
-      sha256 "12344bb9ca95d724e2a1e483e19a494aeb9f50f0fdad789103c5f8c15ae0a1da"
-    else
-      url "https://github.com/OseimuohanI/MailHog/releases/download/v2.0.0/MailHog-linux-amd64"
-      sha256 "ef9e380600477ad82fad5add43117f286ede7a09ea5f51c37e789e00ef2b78b1"
-    end
-  end
+  depends_on "go" => :build
 
   def install
-    bin.install "MailHog-darwin-arm64" => "MailHog" if OS.mac? && Hardware::CPU.arm?
-    bin.install "MailHog-darwin-amd64" => "MailHog" if OS.mac? && Hardware::CPU.intel?
-    bin.install "MailHog-linux-arm64" => "MailHog" if OS.linux? && Hardware::CPU.arm?
-    bin.install "MailHog-linux-amd64" => "MailHog" if OS.linux? && Hardware::CPU.intel?
+    system "go", "build", "-o", "MailHog", "."
+    bin.install "MailHog"
   end
 
   def caveats
